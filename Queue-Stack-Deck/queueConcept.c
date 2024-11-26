ac20ea15b6c0ac48881acedbb473181933bc6fe5
MatheusChiovato <139791765+MatheusChiovato@users.noreply.github.com>
2024-11-26 16:59:09 -0300

    nova funcao para exluir um item da lista criada

--- Queue-Stack-Deck/queueConcept.c
+++ Queue-Stack-Deck/queueConcept.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int list[10]={4,7,12,5,8,47,60,34,1,8};
+int list[10]={4,7,12,5,9,47,60,34,1,8};
 int listlength= sizeof(list)/sizeof(list[0]);
 
 int search(int key){
@@ -40,13 +40,39 @@ int insert(int n){
     }
 }
 
+void excluir(int n){
+
+    int pos=search(n);
+
+    if(pos != -1){
+        for (int i = pos; i < listlength; i++){ //faz a varredura de todos os items da lista a partir do valor n informado
+            list[i]=list[i+1]; //joga todos os valores um índice para tras
+        }
+        list[listlength-1]=n; //define o valor do ultimo indice sendo o valor informado que deseja-se excluir
+         listlength--; //diminui em 1 o tamanho da lista excluindo o ultimo indice;
+
+        printf("Item removido\n");
+        for (int i = 0; i < listlength; i++){ 
+            printf(" %d", list[i]);
+        }
+    } else{
+        printf("Este item não existe\n");
+    }
+
+
+}
+
+
+
 
 
 int main(){
-
+    /*
     search(12);
     insert(13);
+    */
 
+    excluir(47);
 
 
     return 0;
