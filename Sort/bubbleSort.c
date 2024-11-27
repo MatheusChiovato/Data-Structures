58a58af3812047ac6530ad9c8bdc5ef57cd599d7
MatheusChiovato <139791765+MatheusChiovato@users.noreply.github.com>
2024-11-27 19:43:08 -0300

    Bubble sort finalizado

--- Sort/bubbleSort.c
+++ Sort/bubbleSort.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int nums[] = {45, 23, 89, 12, 67, 38, 90, 56, 77, 34, 11, 99, 62, 41, 18, 74, 53, 87, 29, 66};
+int nums[] = {42, 17, 89, 23, 56, 78, 12, 34, 90, 65};
 int length= sizeof(nums)/sizeof(nums[0]);
+
+void printArray(int arr[]){         //funcao feita apenas para printar o array quando necessario
+    for(int i=0;i<length;i++){
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+}
 
 void sort(int nums[]){
 
@@ -16,33 +23,39 @@ void sort(int nums[]){
         }
     }
     
-    for(int i=0;i<length;i++){
-        printf(" %d",nums[i]);
-    }
+    printArray(nums);
 }
 
 void reverseSort(int nums[]){
 
-    for (int i = 0; i < length; i++) { 
-        for (int j = i + 1; j < length; j++) { 
-            if (nums[i] < nums[j]) { 
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+    for(int i=0;i<length;i++){  
+        for(int j=i+1;j<length;j++){ //faz um loop dentro de cada posicao do array para comparar se o numero posterior a i é menor
+        if(nums[i]<nums[j]){    //faz a troca da posicao dos valores jogando o maior para o começo
+            int maior=nums[i];  //foi feito apenas a inversão do sinal na condição do if
+            nums[i]=nums[j];
+            nums[j]=maior;
             }
         }
     }
     
-    for(int i=0;i<length;i++){
-        printf(" %d",nums[i]);
-    }
+   printArray(nums);
 }
 
 
 
 int main(){
 
+    printf("Array desordenado: \n");
+    printArray(nums);
+    printf("\n");
+
+    printf("Array ordenado do MENOR -> MAIOR: \n");
     sort(nums);
+    printf("\n");
+    
+    printf("Array ordenado do MAIOR -> MENOR: \n");
+    reverseSort(nums);
+
     printf("\n");
 
     return 0;
