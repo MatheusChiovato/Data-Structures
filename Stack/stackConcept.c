c4b4777c8f63012162a56f1446670ec9613e85e9
MatheusChiovato <139791765+MatheusChiovato@users.noreply.github.com>
2024-12-13 01:20:42 -0300

    Correção do inicio de Stack

--- Stack/stackConcept.c
+++ Stack/stackConcept.c
@@ -1,40 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int stack[3]={10,20,30};
-int length=sizeof(stack)/sizeof(stack[0]); //aponta o valor 3 indicando o tamanho
-int topo=(sizeof(stack)/sizeof(stack[0])-1); //aponta o valor 2 indicando a ultima posição
+int MAX_STACK=5;
+int stack[5]={10,20,30};
+int top= 2;
 
-void printArray(int arr[]){
-    for(int i=0;i<length;i++){
-        printf(" %d", arr[i]);
+void printStack(){
+    for(int i=0;i<MAX_STACK;i++){
+        printf(" %d", stack[i]);
     }
-        printf("\n");
+    printf("\n");
 }
 
 
-int pushStack(int n){
+int push(int n){
 
-    if(stack[topo] == NULL){    //verifica se a última posição da pilha esta vazia
- 
-        stack[topo]=n; //adiciona o numero desejado
-
-        printArray(stack);
+    if(top<(MAX_STACK-1)){
+        top++;
+        stack[top] = n;
+        return 1;
     } else{
-        printf("Pilha cheia!");
+        return 0;
     }
-    
+
+
 }
+
+int pop(){
+
+    if(top>=0){
+        int val = stack[top];
+        top--;
+        return val;
+    }else{
+        return -1;
+    }
+
+}
+
+
 
 
 
 int main(){
+    printf("\n");
+    if(push(40)){
+        printf("Numero adicionado!\n");
+    }else{
+        printf("Erro ao adicionar o numero, lista cheia!\n");
+    }
+    printf("\n");
+
+    if(push(50)){
+        printf("Numero adicionado!\n");
+    }else{
+        printf("Erro ao adicionar o numero, lista cheia!\n");
+    }
+    printf("\n");
+
+    if(push(60)){
+        printf("Numero adicionado!\n");
+    }else{
+        printf("Erro ao adicionar o numero, lista cheia!\n");
+    }
+    printf("\n");
+
+    printStack();
 
     printf("\n");
-    pushStack(40);
-    printf("\n");
+    printf("Item removido: %d\n", pop());
     
+    printf("\n");
+    printf("Item removido: %d\n", pop());
 
+    printf("\n");
 
     return 0;
 }
